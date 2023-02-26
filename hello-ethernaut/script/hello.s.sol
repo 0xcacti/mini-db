ae4a300b17083c047ee266a31865f12015c2f0f2
0xcacti <0xkelpie@protonmail.com>
2023-02-25 23:38:50 -0500

    set up

---
+++ hello-ethernaut/script/hello.s.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+
+contract MyScript is Script {
+    function run() external {
+        address instance = 0x831AE6eAB78a9D985fD3Ae6d871CA37F93937963;
+        string memory password = "ethernaut0";
+        (bool success, bytes memory data) = instance.call(abi.encodeWithSignature("authenticate(string)", password));
+        require(success, "call failed");
+    }
+}
