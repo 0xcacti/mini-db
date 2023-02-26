4b122afc29451021d204ef48f361ac0042c98739
0xcacti <0xkelpie@protonmail.com>
2023-02-25 23:44:02 -0500

    got script working

--- hello-ethernaut/script/hello.s.sol
+++ hello-ethernaut/script/hello.s.sol
@@ -5,8 +5,10 @@ import "forge-std/Script.sol";
 
 contract MyScript is Script {
     function run() external {
+
         address instance = 0x831AE6eAB78a9D985fD3Ae6d871CA37F93937963;
         string memory password = "ethernaut0";
+        vm.broadcast();
         (bool success, bytes memory data) = instance.call(abi.encodeWithSignature("authenticate(string)", password));
         require(success, "call failed");
     }
