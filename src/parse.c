1840fe0d07dc22239c7fb8ee08d04f63d05205aa
0xcacti <0xcacti@gmail.com>
2025-09-02 15:37:50 -0400

    save

--- src/parse.c
+++ src/parse.c
@@ -9,27 +9,40 @@
 #include "common.h"
 #include "parse.h"
 
-void list_employees(struct dbheader_t *dbhdr, struct employee_t **employees) {
+// void list_employees(struct dbheader_t *dbhdr, struct employee_t **employees)
+// {
+//   if (!dbhdr || !employees) {
+//     fprintf(stderr, "Invalid argument(s) to list_employees\n");
+//     return;
+//   }
+//
+//   if (dbhdr->count == 0) {
+//     printf("No employees to list.\n");
+//     return;
+//   }
+//
+//   if (!*employees) {
+//     fprintf(stderr, "Employee array is NULL but count is %d\n",
+//     dbhdr->count); return;
+//   }
+//
+//   for (int i = 0; i < dbhdr->count; i++) {
+//     printf("Employee %d:\n", i);
+//     printf("\tName: %s\n", (*employees)[i].name);
+//     printf("\tAddress: %s\n", (*employees)[i].address);
+//     printf("\tHours: %u\n", (*employees)[i].hours);
+//   }
+// }
+void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees) {
   if (!dbhdr || !employees) {
-    fprintf(stderr, "Invalid argument(s) to list_employees\n");
-    return;
-  }
-
-  if (dbhdr->count == 0) {
-    printf("No employees to list.\n");
-    return;
-  }
-
-  if (!*employees) {
-    fprintf(stderr, "Employee array is NULL but count is %d\n", dbhdr->count);
     return;
   }
 
   for (int i = 0; i < dbhdr->count; i++) {
     printf("Employee %d:\n", i);
-    printf("\tName: %s\n", (*employees)[i].name);
-    printf("\tAddress: %s\n", (*employees)[i].address);
-    printf("\tHours: %u\n", (*employees)[i].hours);
+    printf("\tName: %s\n", employees[i].name);
+    printf("\tAddress: %s\n", employees[i].address);
+    printf("\tHours: %u\n", employees[i].hours);
   }
 }
 
