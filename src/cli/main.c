9f05ea0bc4e1aafef53858ed59d4c34b9bb58853
0xcacti <0xcacti@gmail.com>
2025-11-11 17:18:01 -0700

    clean and compile and run

--- src/cli/main.c
+++ src/cli/main.c
@@ -1,3 +1,4 @@
+#include "common.h"
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,7 +6,28 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-void send_hello(int fd) {
+int send_hello(int fd) {
+  char buf[4096] = { 0 };
+
+  dbproto_hdr_t *hdr = (dbproto_hdr_t *)buf;
+  hdr->type = htonl((uint16_t)MSG_HELLO_REQ);
+  hdr->length = htons(1);
+
+  dbproto_hello_req_t *req = (dbproto_hello_req_t *)&hdr[1];
+  req->proto = htons(PROTO_VER);
+
+  write(fd, buf, sizeof(dbproto_hdr_t) + sizeof(dbproto_hello_req_t));
+  read(fd, buf, sizeof(buf));
+  hdr->type = ntohl(hdr->type);
+  hdr->length = ntohs(hdr->length);
+  if (hdr->type == MSG_ERROR) {
+    printf("err: protocol mismatch");
+    close(fd);
+    return STATUS_ERROR;
+  }
+
+  printf("Connected to server on protocol: v%d\n", PROTO_VER);
+  return STATUS_SUCCESS;
 }
 
 int main(int argc, char *argv[]) {
@@ -31,7 +53,12 @@ int main(int argc, char *argv[]) {
     exit(EXIT_FAILURE);
   }
 
-  send_helo(fd);
+  int s = send_hello(fd);
+  if (s != 0) {
+    close(fd);
+    exit(EXIT_FAILURE);
+  }
   close(fd);
+
   return 0;
 }
