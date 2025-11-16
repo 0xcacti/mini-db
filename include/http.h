9806161d9a186a139d59c0e5e85124ee2718cb7c
0xcacti <0xcacti@gmail.com>
2025-11-16 00:36:11 -0700

    save

--- include/http.h
+++ include/http.h
@@ -5,6 +5,16 @@
 #define HTTP_PATH_MAX_LEN 2048   // Practical limit for URIs
 #define HTTP_PROTOCOL_MAX_LEN 16 // Standard protocol length (e.g., HTTP/1.1)
 
+typedef enum {
+    HTTP_METHOD_GET,
+    HTTP_METHOD_POST,
+    HTTP_METHOD_PUT,
+    HTTP_METHOD_DELETE,
+    HTTP_METHOD_HEAD,
+    HTTP_METHOD_OPTIONS,
+    HTTP_METHOD_PATCH,
+    HTTP_METHOD_UNKNOWN
+} http_method_e;
 
 typedef struct {
     char method[HTTP_METHOD_MAX_LEN];
