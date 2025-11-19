152d6514ea17a1162f553600c1ca8c8e690b10ed
0xcacti <0xcacti@gmail.com>
2025-11-18 19:32:10 -0700

    save

--- src/http.c
+++ src/http.c
@@ -5,18 +5,19 @@
 #include <unistd.h>
 
 http_parse_e read_http_request(int socket_fd, http_request *request) {
-  char buffer[8192] = { 0 };
-  ssize_t bytes_read = read(socket_fd, buffer, sizeof(buffer) - 1);
+  // char buffer[8192] = { 0 };
+  // ssize_t bytes_read = read(socket_fd, buffer, sizeof(buffer) - 1);
 
-  if (bytes_read <= 0) {
-    return HTTP_PARSE_INVALID;
-  }
+  // if (bytes_read <= 0) {
+  //   return HTTP_PARSE_INVALID;
+  // }
 
-  buffer[bytes_read] = '\0';
+  // buffer[bytes_read] = '\0';
 
-  if (sscanf(buffer, "%7s %2047s %15s", request->method, request->path, request->protocol) != 3) {
-    return HTTP_PARSE_INVALID;
-  }
+  // if (sscanf(buffer, "%7s %2047s %15s", request->method, request->path, request->protocol) != 3)
+  // {
+  //   return HTTP_PARSE_INVALID;
+  // }
 
   return HTTP_PARSE_OK;
 }
